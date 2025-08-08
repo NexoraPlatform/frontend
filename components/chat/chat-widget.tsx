8fec5bd26349c512fdd9c3cc67e8317a0cf82ac8
Arsene94 <arsene.claudiu94@gmail.com>
2025-08-08 18:12:32 +0300

    enabled search groups

--- components/chat/chat-widget.tsx
+++ components/chat/chat-widget.tsx
@@ -380,20 +380,20 @@ export function ChatWidget() {
                             {!activeGroup ? (
                                 /* Groups List */
                                 <div className="flex-1 flex flex-col">
-                                    {/*<div className="p-4 border-b">*/}
-                                    {/*    <div className="flex items-center space-x-2 mb-3">*/}
-                                    {/*        <div className="relative flex-1">*/}
-                                    {/*            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4" />*/}
-                                    {/*            <Input*/}
-                                    {/*                placeholder="Caută conversații..."*/}
-                                    {/*                value={searchTerm}*/}
-                                    {/*                onChange={(e) => setSearchTerm(e.target.value)}*/}
-                                    {/*                className="pl-10"*/}
-                                    {/*            />*/}
-                                    {/*        </div>*/}
-                                    {/*        <CreateGroupDialog onGroupCreated={refreshGroups} />*/}
-                                    {/*    </div>*/}
-                                    {/*</div>*/}
+                                    <div className="p-4 border-b">
+                                        <div className="flex items-center space-x-2 mb-3">
+                                            <div className="relative flex-1">
+                                                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4" />
+                                                <Input
+                                                    placeholder="Caută conversații..."
+                                                    value={searchTerm}
+                                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                                    className="pl-10"
+                                                />
+                                            </div>
+                                            {/*<CreateGroupDialog onGroupCreated={refreshGroups} />*/}
+                                        </div>
+                                    </div>
 
                                     <ScrollArea className="flex-1" >
                                         <div className="p-2 space-y-1">
