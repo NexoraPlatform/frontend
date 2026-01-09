35e08c72d22a32d2f380c4ca0f8a310d9d09e7f2
Arsene94 <arsene.claudiu94@gmail.com>
2026-01-09 11:32:49 +0200

    Allow local image host

--- next.config.js
+++ next.config.js
@@ -43,6 +43,10 @@ const nextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
       }
     ],
     formats: ['image/avif', 'image/webp'],
