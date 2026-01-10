4f50ccdbb0ed540816aa01a78f53439c787e8796
Arsene94 <arsene.claudiu94@gmail.com>
2026-01-10 14:52:41 +0200

    Animate hero fallback and align React versions

--- components/hero/TrustFlowNetwork3D.tsx
+++ components/hero/TrustFlowNetwork3D.tsx
@@ -7,8 +7,9 @@ function TrustFlowFallback() {
     return (
         <div className="absolute inset-0 bg-gradient-to-br from-[#0B1C2D] via-[#0F2438] to-[#0B1C2D]">
             <div className="absolute inset-0 opacity-30">
-                <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-[#1BC47D] rounded-full blur-[100px]" />
-                <div className="absolute bottom-1/4 right-1/4 w-64 h-64 bg-[#3B82F6] rounded-full blur-[100px]" />
+                <div className="absolute -inset-1/3 rounded-full border border-[#1BC47D]/20 animate-[spin_60s_linear_infinite]" />
+                <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-[#1BC47D] rounded-full blur-[100px] animate-[pulse_8s_ease-in-out_infinite]" />
+                <div className="absolute bottom-1/4 right-1/4 w-64 h-64 bg-[#3B82F6] rounded-full blur-[100px] animate-[pulse_10s_ease-in-out_infinite]" />
             </div>
         </div>
     );
