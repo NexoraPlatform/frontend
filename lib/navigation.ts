7551851e6ce974143bc7bd220b99cc533e9108ee
Arsene94 <arsene.claudiu94@gmail.com>
2026-01-21 13:05:12 +0200

    fix: use next-intl navigation routing helpers

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -1,7 +1,12 @@
-import {createSharedPathnamesNavigation} from 'next-intl/navigation';
+import {createNavigation} from 'next-intl/navigation';
+import {defineRouting} from 'next-intl/routing';
+import {defaultLocale} from '@/lib/i18n';
 
 export const locales = ['en', 'ro'] as const;
 
-export const {Link, redirect, usePathname, useRouter} = createSharedPathnamesNavigation({
+export const routing = defineRouting({
     locales,
+    defaultLocale,
 });
+
+export const {Link, redirect, usePathname, useRouter} = createNavigation(routing);
