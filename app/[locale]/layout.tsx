1390c4c2c33bcfdb4f36a3fd8fdf78c002c0ed56
Arsene94 <arsene.claudiu94@gmail.com>
2026-04-04 20:15:18 +0300

    add nonce to theme provider

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -88,6 +88,7 @@ export default async function LocaleLayout({ children, params }: Props) {
               enableSystem
               disableTransitionOnChange
               storageKey={TRUSTORA_THEME_STORAGE_KEY}
+              nonce={nonce}
             >
               <AppShell>{children}</AppShell>
             </ThemeProvider>
